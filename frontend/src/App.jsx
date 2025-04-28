a289ef4d39b50879bd779c10b52d57121a86a391
Mohotti5353 <168615486+Mohotti5353@users.noreply.github.com>
2025-04-28 22:12:30 +0530

    adding route paths to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import Post from "./components/Post.jsx";
+import MyPosts from "./components/MyPosts.jsx";
+import SharedPosts from "./components/SharedPosts.jsx";
+import MyProfile from "./components/MyProfile.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <GoogleOAuthProvider clientId="507608362836-3qe3nq4n5900e3559f1v6udf4mk47g2q.apps.googleusercontent.com">
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Post />} />
+                    <Route path="/shared" element={<SharedPosts />} />
+                    <Route path="/dashboard" element={<Post />} />
+                    <Route path="/post" element={<Post />} />
+                    <Route path="/mypost" element={<MyPosts />} />
+                    <Route path="/my-profile" element={<MyProfile/>} />
+                    <Route path="/" element={<Login  />} />
+                </Routes>
+            </Router>
+        </GoogleOAuthProvider>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
