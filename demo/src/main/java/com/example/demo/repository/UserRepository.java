3aa61421f5e14281ef94259802b34cf35918b72a
Mohotti5353 <168615486+Mohotti5353@users.noreply.github.com>
2025-04-29 07:13:42 +0530

    create user.java model for login

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username); // For checking existing username during login
-}
