079bf04112918e410024b50640f6149275b2f5bb
Kanchana Koralage <itsmekanchanakoralage@gmail.com>
2025-05-19 13:22:19 +0530

    Feedbackrepository updated

--- demo/src/main/java/com/example/demo/repository/FeedbackRepository.java
+++ demo/src/main/java/com/example/demo/repository/FeedbackRepository.java
@@ -9,5 +9,7 @@ import com.example.demo.model.Feedback;
 @Repository
 public interface FeedbackRepository extends MongoRepository<Feedback,String>{
 
-
+    // Custom query methods can be defined here if needed
+    // For example, to find feedback by userId:
+    // List<Feedback> findByUserId(String userId);
 }
